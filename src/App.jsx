6744b353eafbbba6d1cf1fb3f86fa5cad65932bd
forersa <forersa@yahoo.com>
2024-03-28 18:32:54 +0100

    Config new app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 function App() {
-    return <h1 className="text-3xl font-bold underline bg-orange-500">
-    Hello world!
+    return ( 
+    <h1 className="text-3xl font-bold underline bg-orange-500">
+      Hello world!
+      <div className="bg-blue-300 flex justify-between">
+    
+        <div className="bg-red-200 w-fit"> soy el hijo</div>
+    
+        <div className="bg-red-500 w-fit"> soy el hijo</div>
+      </div>
   </h1>
+  
+    )
 }
 
 export default App
